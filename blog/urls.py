c49901341851ce96ca1a351116a00b90136de867
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-12 00:53:44 -0700

    add templates and styles

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.posts, name="posts"),
-    path("posts/<slug>", views.post, name="post"),
+    path("posts/<slug:slug>", views.post_detail, name="post"),
 ]
