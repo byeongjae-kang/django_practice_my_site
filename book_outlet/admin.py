228fd6e4a9315a7fd3a19064197b43d454e58f67
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-12 15:27:16 -0700

    configure admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
+
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = (
+        "title",
+        "rating",
+    )
+
+    list_display = ("title", "rating")
+
+
+admin.site.register(Book, BookAdmin)
