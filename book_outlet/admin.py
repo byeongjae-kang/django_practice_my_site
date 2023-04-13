7be18d078b1f7e1525acc89206ed931a750b457d
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-13 03:01:41 -0700

    add one to one and one to many relationship

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
-from .models import Book
+from .models import Address, Book, Author
 
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = (
-        "title",
+        "author",
         "rating",
     )
 
-    list_display = ("title", "rating")
+    list_display = ("title", "author")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+    )
+    list_filter = (
+        "first_name",
+        "last_name",
+    )
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,10 +1,37 @@
-from django.core import validators
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    province = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    postal_code = models.CharField(max_length=6, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.street}, {self.city}, {self.province}, {self.country} ,{self.postal_code}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    address = models.ForeignKey(
+        Address, related_name="author", on_delete=models.CASCADE, null=True
+    )
+
+    def full_name(self):
+        return f"{self.first_name}, {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
 
 
 class Book(models.Model):
@@ -12,7 +39,9 @@ class Book(models.Model):
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books"
+    )
     is_bestselling = models.BooleanField(default=False)
     # Harry Potter 1 => harry-potter-1
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
