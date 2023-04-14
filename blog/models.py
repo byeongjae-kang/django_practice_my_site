60e2b74d99b75fa4a1d05f3c2bdde67e380ee4c0
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-13 23:28:18 -0700

    update models and add config admin

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 
 # Create your models here.
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name}, {self.last_name}"
 
     def __str__(self) -> str:
-        return f"{self.first_name}, {self.last_name}"
+        return self.full_name()
 
 
 class Tag(models.Model):
@@ -24,9 +28,13 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     excerpt = models.CharField(max_length=50)
     image = models.CharField(max_length=50)
-    date = models.DateField(auto_now_add=True)
-    slug = models.SlugField(blank=True)
-    content = models.CharField(max_length=1000)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, null=True, related_name="post", on_delete=models.SET_NULL
+    )
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
         return f"{self.title}, {self.slug}"
